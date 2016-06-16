26c35ac93beb71da7cc4c665f7f637fc1c8cd3d1
Freddie Witherden <freddie@witherden.org>
2016-06-16 11:01:35 -0700

    Generator updates.

--- gimmik/__init__.py
+++ gimmik/__init__.py
@@ -20,8 +20,33 @@ def generate_mm(mat, dtype, platform, alpha=1.0, beta=0.0, tol=1e-10,
     else:
         raise ValueError('Invalid floating point data type')
 
-    # Multiply the matrix through by alpha
-    mat = alpha*mat
+    # Multiply the matrix through by alpha and clean it up
+    mat = _clean(alpha*mat, tol)
+
+    # Template arguments
+    tplargs = {'dtype': dtype, 'mat': mat, 'beta': beta, 'funcn': funcn}
+
+    if platform == 'c-omp':
+        tplargs['tilings'] = _tile(mat, sizes=[1])
+    elif platform == 'opencl':
+        tplargs['tiling'] = _partition(mat, 2)
+
+    # Load and render the template
+    tpl = pkgutil.get_data(__name__, 'kernels/{0}.mako'.format(platform))
+    src = Template(tpl).render(**tplargs)
+
+    # At single precision suffix all floating point constants by 'f'
+    if dtype == 'float':
+        src = re.sub(r'(?=\d*[.eE])(?=\.?\d)\d*\.?\d*(?:[eE][+-]?\d+)?',
+                     r'\g<0>f', src)
+
+    # Return the source
+    return src
+
+
+def _clean(mat, tol):
+    # Copy the matrix
+    mat = mat.copy()
 
     # Clamp small elements
     mat[abs(mat) < tol] = 0
@@ -43,14 +68,34 @@ def generate_mm(mat, dtype, platform, alpha=1.0, beta=0.0, tol=1e-10,
     # Fix up the signs and assign
     mat.flat = np.copysign(amfl, mat.flat)
 
-    # Load and render the template
-    tpl = pkgutil.get_data(__name__, 'kernels/{0}.mako'.format(platform))
-    src = Template(tpl).render(dtype=dtype, mat=mat, beta=beta, funcn=funcn)
+    return mat
 
-    # At single precision suffix all floating point constants by 'f'
-    if dtype == 'float':
-        src = re.sub(r'(?=\d*[.eE])(?=\.?\d)\d*\.?\d*(?:[eE][+-]?\d+)?',
-                     r'\g<0>f', src)
 
-    # Return the source
-    return src
+def _tile(mat, sizes=[1, 2, 3, 5, 7, 8], tol=0.1):
+    tilings = {}
+
+    for n in sizes:
+        tiling = _partition(mat, n)
+
+        minsz = min(end - start for start, end in tiling)
+        maxsz = max(end - start for start, end in tiling)
+
+        if len(tiling) == n and maxsz / minsz - 1 < tol:
+            tilings[n] = tiling
+
+    return tilings
+
+
+def _partition(mat, n):
+    idx = [0]
+    num = np.count_nonzero(mat) // n
+    cnt = 0
+
+    for i, row in enumerate(mat):
+        if cnt >= num:
+            idx.append(i)
+            cnt = 0
+
+        cnt += np.count_nonzero(row)
+
+    return list(zip(idx, idx[1:] + [i + 1]))
--- gimmik/kernels/opencl.mako
+++ gimmik/kernels/opencl.mako
@@ -6,26 +6,39 @@
 #endif
 % endif
 
+% for q, (start, end) in enumerate(tiling)
+void
+${funcn}_${q}(int i,
+              const ${dtype}* restrict b, int ldb,
+              ${dtype}* restrict c, int ldc)
+{
+    ${dtype} dotp;
+
+% for j, jx in enumerate(mat[start:end], start=start):
+    dotp = ${' + '.join('{kx}*b[i + {k}*ldb]'.format(k=k, kx=kx)
+                        for k, kx in enumerate(jx) if kx != 0)};
+% if beta == 0:
+    c[i + ${j}*ldc] = dotp;
+% elif beta == 1:
+    c[i + ${j}*ldc] += dotp;
+% else:
+    c[i + ${j}*ldc] = dotp + ${beta}*c[i + ${j}*ldc];
+% endif
+% endfor
+}
+% endfor
+
 __kernel void
 ${funcn}(int n,
-         __global const ${dtype}* restrict b, int ldb,
-         __global ${dtype}* restrict c, int ldc)
+         const ${dtype}* restrict b, int ldb,
+         ${dtype}* restrict c, int ldc)
 {
     int i = get_global_id(0);
-    ${dtype} dotp;
 
     if (i < n)
     {
-    % for j, jx in enumerate(mat):
-        dotp = ${' + '.join('{kx}*b[i + {k}*ldb]'.format(k=k, kx=kx)
-                            for k, kx in enumerate(jx) if kx != 0)};
-    % if beta == 0:
-        c[i + ${j}*ldc] = dotp;
-    % elif beta == 1:
-        c[i + ${j}*ldc] += dotp;
-    % else:
-        c[i + ${j}*ldc] = dotp + ${beta}*c[i + ${j}*ldc];
-    % endif
+    % for q in range(len(tiling)):
+        ${funcn}_${q}(i, b, ldb, c, ldc);
     % endfor
     }
 }
