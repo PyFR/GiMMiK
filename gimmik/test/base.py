678482825e906306e8aa9cca76125d713b529b2d
Will Trojak <wt247@tamu.edu>
2021-07-23 13:57:35 -0500

    Fixed test malloc.

--- gimmik/test/base.py
+++ gimmik/test/base.py
@@ -5,10 +5,8 @@ from pyfr.backends import get_backend
 
 
 class BaseTest(object):
-    def __init__(self, platform):
+    def __init__(self, platform, cfg):
         self.platform = platform
-
-        cfg = default_cfg()
         self.cfg = cfg
 
         backend = get_backend(platform, cfg)
@@ -17,11 +15,13 @@ class BaseTest(object):
     def mul_time_test(self):
         pass
 
-    def test_malloc(self, mat, n=10):
+    def test_malloc(self, mat, n=1024):
         backend = self.backend
 
         (n1, n2) = np.shape(mat)
         n0 = n*backend.soasz
 
-        self._xin = backend.matrix((n0, n2), 1, tags={'align'})
-        self._xout = backend.matrix((n0, n1), 2, tags={'align'})
+        print(f'{n0=}, {n1=}, {n2=}')
+
+        self._xin = backend.matrix((n0, n2), tags={'align'})
+        self._xout = backend.matrix((n0, n1), tags={'align'})
