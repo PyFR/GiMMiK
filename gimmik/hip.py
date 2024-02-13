07220ea8100d6ffa908abac5e9128df56af0a6f0
Freddie Witherden <freddie@witherden.org>
2024-02-13 16:12:12 -0600

    Disable vector kernels with HIP due to compiler bugs.

--- gimmik/hip.py
+++ gimmik/hip.py
@@ -26,30 +26,6 @@ class HIPMatMul(MatMul):
         meta = {'block': (blkx, ks, 1), 'shared': (ks - 1)*csz*blkx*dsize}
         yield ('cstream-ksplit', args, meta)
 
-        # At single precision also consider vectorized kernels
-        if (dtype == 'float' and
-            self.aligne is not None and self.aligne % 2 == 0):
-            # Vector B loading, C streaming kernel
-            args = {'dtype': 'float2', 'width': 2}
-            meta = {'width': 2}
-            yield ('cstream', args, meta)
-
-            # Vector four-way m-split B streaming, C accumulation kernel
-            ms, bsz, blkx = 4, 16, 64
-            args = {'dtype': 'float2', 'width': 2, 'msplit': ms,
-                    'bsz': bsz, 'blockx': blkx}
-            meta = {'block': (blkx, ms, 1), 'width': 2,
-                    'shared': 2*blkx*bsz*2*dsize}
-            yield ('bstream-msplit', args, meta)
-
-            # Vector two-way k-split B loading, C streaming kernel
-            ks, csz, blkx = 2, 24, 64
-            args = {'dtype': 'float2', 'width': 2, 'ksplit': ks,
-                    'csz': csz, 'blockx': blkx}
-            meta = {'block': (blkx, ks, 1), 'width': 2,
-                    'shared': 2*(ks - 1)*csz*blkx*dsize}
-            yield ('cstream-ksplit', args, meta)
-
     def _process_meta(self, meta):
         if self.n is not None:
             div = meta['block'][0]*meta['width']
