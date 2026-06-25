f6bc30882c7fe8c36da58d4763a2b32089b13a49
tomjen12 <tomjen12@amd.com>
2026-06-25 02:02:37 -0500

    Prune HIP tuned variants to 12 Reduce the HIP tuned kernel search space from 28 variants to 12 and order the remaining variants to try common winners earlier.

--- gimmik/hip.py
+++ gimmik/hip.py
@@ -24,52 +24,23 @@ class HIPMatMul(MatMul):
 
         blkx = self.basemeta['block'][0]
 
-        # B loading, C streaming kernel
-        yield from emit('cstream', {'blockx': blkx}, {})
-
-        # B streaming, C accumulation kernel
-        yield from emit('bstream', {'blockx': blkx}, {})
-
-        # Four-way m-split B streaming, C accumulation kernel
-        ms, bsz, blkx = 4, 24, 64
-        args = {'msplit': ms, 'bsz': bsz, 'blockx': blkx}
-        meta = {'block': (blkx, ms, 1), 'shared': 2*bsz*blkx*dsize}
-        yield from emit('bstream-msplit', args, meta)
-
-        # Two-way k-split B loading, C streaming kernel
-        ks, csz, blkx = 2, 24, 64
-        args = {'ksplit': ks, 'csz': csz, 'blockx': blkx}
-        meta = {'block': (blkx, ks, 1), 'shared': (ks - 1)*csz*blkx*dsize}
-        yield from emit('cstream-ksplit', args, meta)
-
         # Only emit tuned variants on architectures they have been validated for.
         base_arch = gcn_arch.split(':', 1)[0] if gcn_arch else None
         if base_arch not in {'gfx90a', 'gfx942'} or warp_size != 64:
             return
 
         # Tuned HIP variants
-        msplits, ksplits = [4, 8], [2, 4]
+        msplits, ksplits = [8, 4], [4, 2]
         bsz, csz, blkx = 8, 8, 64
         widths = [1]
         if self.aligne is not None and self.aligne % 2 == 0:
-            widths.append(2)
+            widths.insert(0, 2)
 
         for width in widths:
             wargs = ({'dtype': f'{dtype}{width}', 'width': width}
                      if width > 1 else {})
             wmeta = {'width': width} if width > 1 else {}
             wpfx = f'w{width}-' if width > 1 else ''
-
-            # B loading, C streaming kernel
-            args = {'blockx': blkx} | wargs
-            meta = {'block': (blkx, 1, 1),
-                    'desc': f'cstream/{wpfx}x{blkx}'} | wmeta
-            yield from emit('cstream', args, meta)
-
-            # B streaming, C accumulation kernel
-            meta = {'block': (blkx, 1, 1),
-                    'desc': f'bstream/{wpfx}x{blkx}'} | wmeta
-            yield from emit('bstream', args, meta)
 
             for ms in msplits:
                 # m-split B streaming, C accumulation kernel
@@ -80,27 +51,6 @@ class HIPMatMul(MatMul):
                     'desc': f'bstream-msplit/{wpfx}m{ms}-b{bsz}-x{blkx}'
                 } | wmeta
                 yield from emit('bstream-msplit', args, meta)
-
-            for ks in ksplits:
-                # k-split B loading, C streaming kernel
-                args = {'ksplit': ks, 'csz': csz, 'blockx': blkx} | wargs
-                shared = (ks - 1)*csz*blkx*dsize*width
-                meta = {
-                    'block': (blkx, ks, 1), 'shared': shared,
-                    'desc': f'cstream-ksplit/{wpfx}k{ks}-c{csz}-x{blkx}'
-                } | wmeta
-                yield from emit('cstream-ksplit', args, meta)
-
-            # B loading, C preloading, C streaming kernel
-            args = {'blockx': blkx} | wargs
-            meta = {'block': (blkx, 1, 1),
-                    'desc': f'cstream-preload-c/{wpfx}x{blkx}'} | wmeta
-            yield from emit_preload('cstream', args, meta)
-
-            # B streaming, C preloading, C accumulation kernel
-            meta = {'block': (blkx, 1, 1),
-                    'desc': f'bstream-preload-c/{wpfx}x{blkx}'} | wmeta
-            yield from emit_preload('bstream', args, meta)
 
             for ms in msplits:
                 # m-split B streaming, C preloading, C accumulation kernel
